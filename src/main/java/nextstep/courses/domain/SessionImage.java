04823e754792e65c80ae3d7816dac30cf408509c
sinyebin <dpqls0625@naver.com>
2024-10-31 14:21:00 +0900

    refactor: img -> image로 변경

--- src/main/java/nextstep/courses/domain/SessionImage.java
+++ src/main/java/nextstep/courses/domain/SessionImage.java
@@ -8,17 +8,17 @@ public class SessionImage {
     private static final String INVALID_DIMENSIONS_MESSAGE = "이미지 크기는 최소 300x200 이상이어야 합니다.";
     private static final String INVALID_ASPECT_RATIO_MESSAGE = "이미지 비율은 3:2여야 합니다.";
     private static final String INVALID_IMG_TYPE = "유효하지 않은 이미지 타입입니다.";
-    private final int imgSize;
-    private final int imgWidth;
-    private final int imgHeight;
+    private final int imageSize;
+    private final int imageWidth;
+    private final int imageHeight;
     private final ImageType imageType;
 
-    public SessionImage(int imgSize, int imgWidth, int imgHeight, String type) {
-        validateImgSize(imgSize);
-        validateImageDimension(imgWidth, imgHeight);
-        this.imgSize = imgSize;
-        this.imgWidth = imgWidth;
-        this.imgHeight = imgHeight;
+    public SessionImage(int imageSize, int imageWidth, int imageHeight, String type) {
+        validateImgSize(imageSize);
+        validateImageDimension(imageWidth, imageHeight);
+        this.imageSize = imageSize;
+        this.imageWidth = imageWidth;
+        this.imageHeight = imageHeight;
         this.imageType = ImageType.fromString(type);
     }
 
